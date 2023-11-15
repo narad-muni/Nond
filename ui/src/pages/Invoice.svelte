59c9d21697f86870ffafe06da0f25509b8d36525
Motabhai <admin@Saumils-MacBook-Pro.local>
2023-11-15 11:33:02 +0530

    mail error and success msg at same time

--- ui/src/pages/Invoice.svelte
+++ ui/src/pages/Invoice.svelte
@@ -320,19 +320,18 @@
         if(resp.status == 'success'){
             const data = resp.data;
             let err_msg = "";
-
-            if(data?.filter(e => e.success == false)?.length == 0){
-                success = "Mail sent successfully";
-                return
-            }
+            let success_msg = "";
 
             data.forEach(e => {
                 if(e.success == false){
                     err_msg += `<br/>${e.message}`
+                }else{
+                    success_msg += `<br/>${e.message}`
                 }
             });
 
             error = err_msg;
+            success = success_msg;
 
         }else{
             error = resp.message;
@@ -826,7 +825,8 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
                 </svg>
             </span>
-            <span class="font-medium">success!</span> {success}
+            <span class="font-medium">success!</span>
+            {@html success}
         </Alert>
     </div>
 {/if}
