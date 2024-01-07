cbe6b116f3f73f9d369ec8fae7d924d57a5e0861
Motabhai <admin@Saumils-MacBook-Pro.local>
2024-01-07 11:41:38 +0530

    Minor fix

--- ui/src/pages/Company.svelte
+++ ui/src/pages/Company.svelte
@@ -384,16 +384,16 @@
                                             {#if header.column_type == 'Text'}
                                                 {row[header.column_name] || "-"}
                                             {:else if header.column_type == 'File'}
-                                                {#if row[header.column_name]}
-                                                    <A target="_blank" href={row[header.column_name]}>
+                                                {#if row[header.column_name]?.path}
+                                                    <A target="_blank" href={row[header.column_name]?.path}>
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                                             <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                                                         </svg>
                                                         &nbsp;
-                                                        {header.display_name}
+                                                        {row[header.column_name]?.value || "-"}
                                                     </A>
                                                 {:else}
-                                                    -
+                                                    {row[header.column_name]?.value || "-"}
                                                 {/if}
                                             {:else if header.column_type == 'Date'}
                                                 {row[header.column_name] || "-"}
